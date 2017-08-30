f218a12d9640c57f52c702ec4c3041d4e431851a
Thoulah <Thoulah@users.noreply.github.com>
2017-08-30 23:59:48 +0200

    Correcting symlink

--- views/user/recovery/reset.php
+++ views/user/recovery/reset.php
@@ -1,1 +1,25 @@
-./request.php
+<?php
+use yii\bootstrap\{ActiveForm, Html};
+
+$this->title = Yii::t('usuario', 'Reset your password');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="row">
+	<div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3"><?php
+		echo Html::tag('h3', Html::encode($this->title));
+echo Yii::$app->controller->action->id;
+		$form = ActiveForm::begin([
+			'id' => $model->formName(),
+			'enableAjaxValidation' => true,
+			'enableClientValidation' => false,
+		]);
+
+		echo $form->field($model, 'email', [
+			'inputTemplate' => '<div class="input-group"><span class="input-group-addon">'.Html::icon('lock').'</span>{input}</div>',
+		])->passwordInput(['autofocus' => true, 'tabindex' => 1]);
+
+		echo Html::submitButton(Yii::t('usuario', 'Continue'), ['class' => 'btn btn-primary btn-block', 'autofocus' => 2]);
+
+		ActiveForm::end(); ?>
+	</div>
+</div>
