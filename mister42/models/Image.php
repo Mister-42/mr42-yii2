8ec5b65b8a0ecab3122430b159ef4c87bec2a88e
Thoulah <Thoulah@users.noreply.github.com>
2018-04-01 06:38:52 +0200

    Usage of a function in loops should be avoided

--- mister42/models/Image.php
+++ mister42/models/Image.php
@@ -5,8 +5,9 @@ use Yii;
 class Image {
 	public function getAverageColor(string $image): string {
 		$i = imagecreatefromstring($image);
-		for ($x=0; $x < imagesx($i); $x++) {
-			for ($y=0; $y < imagesy($i); $y++) {
+		list($width, $height) = getimagesizefromstring($image);
+		for ($x=0; $x < $width; $x++) {
+			for ($y=0; $y < $height; $y++) {
 				$rgb = imagecolorat($i, $x, $y);
 				$r = ($rgb >> 16) & 0xFF;
 				$g = ($rgb >> 8) & 0xFF;
