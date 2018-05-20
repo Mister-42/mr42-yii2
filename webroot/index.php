60cbc4fdf2b526642ada51e8b425435165999a02
Thoulah <johan@mr42.me>
2018-05-20 21:50:24 +0200

    Rebuild index as a class

--- webroot/index.php
+++ webroot/index.php
@@ -1,22 +1,36 @@
 <?php
-header_remove("X-Powered-By");
-#error_reporting(-1); defined('YII_DEBUG') or define('YII_DEBUG', true);
+class Index {
+	private $debug = 0;
 
-require(__DIR__ . '/../../vendor/autoload.php');
-require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+	public function __construct() {
+		if ($this->debug) :
+			error_reporting(-1);
+			define('YII_DEBUG', true);
+		endif;
 
-$domain = substr($_SERVER['HTTP_HOST'], 0, 4) == 'www.' ? substr($_SERVER['HTTP_HOST'], 4) : $_SERVER['HTTP_HOST'];
-switch ($domain) {
-	case 'mister42.me': $cfg = getConfig(['mr42/mister42']); break;
-	case 'mr42.me': $cfg = getConfig(['mr42/mister42', 'mr42/mr42']); break;
+		header_remove("X-Powered-By");
+		require(__DIR__.'/../../vendor/autoload.php');
+		require(__DIR__.'/../../vendor/yiisoft/yii2/Yii.php');
+	}
+
+	public function getConfig() {
+		switch (substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.' ? substr($_SERVER['HTTP_HOST'], 4) : $_SERVER['HTTP_HOST']) :
+			case 'mister42.me': return $this->loadConfig(['mister42']);
+			case 'mr42.me': return $this->loadConfig(['mister42', 'mr42']);
+		endswitch;
+	}
+
+	private function loadConfig(array $dir): array {
+		$webConfig = $dir[1] ?? $dir[0];
+		return \yii\helpers\ArrayHelper::merge(
+			require(__DIR__.'/../'.$dir[0].'/common.php'),
+			require(__DIR__.'/../'.$webConfig.'/web.php')
+		);
+	}
 }
 
-(new yii\web\Application($cfg))->run();
+$index = new Index();
+$config = $index->getConfig();
 
-function getConfig(array $dir): array {
-	$webConfig = $dir[1] ?? $dir[0];
-	return \yii\helpers\ArrayHelper::merge(
-		require(__DIR__ . '/../../' . $dir[0] . '/common.php'),
-		require(__DIR__ . '/../../' . $webConfig . '/web.php')
-	);
-}
+$run = new yii\web\Application($config);
+$run->run();
