534141d18a11f741e0f01f0f3b20aa2ff1039db4
Thoulah <johan@mr42.me>
2019-02-06 22:49:57 +0100

    Fix: define table name

--- mister42/models/lyrics/LyricsQuery.php
+++ mister42/models/lyrics/LyricsQuery.php
@@ -6,10 +6,10 @@ class LyricsQuery extends \yii\db\ActiveQuery {
 	const STATUS_INACTIVE = '0';
 	const STATUS_ACTIVE = '1';
 
-	public function active($table): self {
+	public function active(string $table): self {
 		if (php_sapi_name() === 'cli' || (!Yii::$app->user->isGuest && Yii::$app->user->identity->isAdmin))
 			return $this->onCondition(['or', [$table.'.active' => [Self::STATUS_INACTIVE, Self::STATUS_ACTIVE]]]);
 
-		return $this->onCondition(['active' => Self::STATUS_ACTIVE]);
+		return $this->onCondition([$table.'.active' => Self::STATUS_ACTIVE]);
 	}
 }
